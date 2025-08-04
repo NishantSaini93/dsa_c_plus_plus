bd0c8858ae46a5a579b4c71b2fcbb580b4d876e7
Nishant Saini <nishantsainins@outlook.com>
2025-08-04 15:56:00 -0400

    Updated the documentation of Hello World program

--- c_plus_plus_basics/hello_world.cpp
+++ c_plus_plus_basics/hello_world.cpp
@@ -1,5 +1,15 @@
 
-// This is a simple C++ program that prints "Hello World!" to the console.
+/**
+ * @file hello_world.cpp
+ * @brief A simple C++ program that demonstrates basic output operations.
+ *
+ * This program prints "Hello World!" and an additional message to the console.
+ * It serves as an introductory example for C++ syntax, usage of the iostream library,
+ * and the structure of a basic C++ application.
+ *
+ * @author Nishant Saini
+ * @date 04 Aug 2025
+ */
 
 // iostream is preprocessor directive that includes the standard input-output stream library
 #include <iostream>
@@ -8,10 +18,15 @@
 using std::cout;
 using std::endl;
 
+/**
+ * @brief Entry point of the program.
+ *
+ * This function starts the execution of the program. It prints
+ * "Hello World!" and an additional message to the standard output.
+ *
+ * @return int Returns 0 upon successful execution.
+ */
 int main() {
-    /*
-    The main function is the entry point of the program.
-    */
     cout<<"Hello World!"<<endl;
     // Alternatively, you can use "\n" for a new line
     // "\n" is often faster than endl because endl also flushes the output buffer
